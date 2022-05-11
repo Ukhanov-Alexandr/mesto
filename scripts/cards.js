607d75b1ec115497a6cd7f1d672420aa64daddfc
Alexandr Ukhanov <ukhanov1996@yandex.ru>
2022-05-12 02:28:13 +0300

    hello jared!

--- scripts/cards.js
+++ scripts/cards.js
@@ -20,7 +20,7 @@ const initialCards = [
     link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
   },
   {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    name: 'Jared',
+    link: 'https://media0.giphy.com/media/l41YwWrjEhTGpE3zG/giphy.gif?cid=790b7611a485b2e4c2c72018ce88b1663564f45827d0a363&rid=giphy.gif&ct=g'
   }
 ];
